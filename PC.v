82e2e718053167773eb8a1707038c8b257f07e9a
mhadih <m.hadi.hojjat@gmail.com>
2019-05-12 19:02:31 +0430

    compile complete

--- PC.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns/1ns
-module PC(input [4:0] pc,input clk, rst, load, output reg [4:0] pcOut);
-  always @(posedge clk , posedge rst) begin
-    if(rst) pcOut <= 10'b0;
-    else if ()pcOut <= pc;
-  end
-endmodule
--- Register.v
+++ Register.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 module Register(input[7:0] in, input clk, rst, load, output reg[7:0] out);
-    always @(posedge clk,posedge rst)
+    always @(posedge clk,posedge rst) begin
         if (rst) out <= 8'b0;
-        else if (load) out <= in
+        else if (load) out <= in;
     end
 endmodule
