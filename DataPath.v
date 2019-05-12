21a3bd27536991a01e28b90e197f304051aa1f44
mhadih <m.hadi.hojjat@gmail.com>
2019-05-12 09:16:09 +0430

    DataPath Argument

---
+++ DataPath.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ns
+module DataPath(input clk,rst,
+                    pcWrite, pcWriteCond, pcSrc, IorD, memRead, memWrite, IRWrite, MtoS
+                    ldA, ldB, srcA, srcB, ALUOp, push, pop, tos
+                    input [1:0] func,
+                output zero,
+                output[2:0] opcode,
+                output[4:0] Addrs);
+    
+
+endmodule
