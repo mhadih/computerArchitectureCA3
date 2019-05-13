77997e8eb69724b68c5eee419d1adbea8c7f20b5
hadiloomehdi <hadiloomehdi@gmail.com>
2019-05-13 20:22:56 +0430

    ready for upload

--- memory.v
+++ memory.v
@@ -7,25 +7,22 @@ module Memory(input [4:0] adr,input [7:0] data,input memwen,memRead,clk,output [
     for (i=0; i<=31;i=i+1) 
       m[i] = 8'b0;
 
-		m[0] = 8'b10011101;	// push 8 in St
-		m[1] = 8'b10011101;	// push 8 in St
-		m[2] = 8'b00000000;	// add 8 + 8 and stoers 16 in St
-		m[3] = 8'b11000111;	// jump to instruction 7
-		m[7] = 8'b11101100;	// does not jumpz to 12 
-		m[8] = 8'b10011110;	// push 16 on St
-		m[9] = 8'b00100000;	// sub 16 - 16 and store 0 on St
-		m[10] = 8'b11101111;	// jumpz 15
-		m[15] = 8'b10011100;	// push 10101010 on St
-		m[16] = 8'b10011011;	// push 01100110 on St
-		m[17] = 8'b01000000;	// and 10101010 & 01100110 and store 01000100 on St
-		m[18] = 8'b01100000;	// not 01000100 and store 10111011 on St
-		m[19] = 8'b10111111;	// pop 10111011 from st and store it on m[31]
-		m[20] = 8'b10111110;	// pop 0 from st and store it on m[30]
+    m[0] = 8'b10011001;
+		m[1] = 8'b10011010;
+		m[2] = 8'b10011011;
+		m[3] = 8'b10011100;
+		m[4] = 8'b10011101;
+		m[5] = 8'b00000000;
+		m[6] = 8'b00000000;
+		m[7] = 8'b00000000;
+		m[8] = 8'b00000000;
+		m[9] = 8'b10111110;
 
-    m[27] = 8'b01100110;
-		m[28] = 8'b10101010;
-		m[29] = 8'b00001000;
-		m[30] = 8'b00010000;
+		m[25] = 8'b00001001;
+		m[26] = 8'b00000111;
+		m[27] = 8'b00000101;
+		m[28] = 8'b00000011;
+		m[29] = 8'b00000001;
 
   end
 
